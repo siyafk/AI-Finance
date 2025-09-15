350884487ee2257c13ccab9838bc60e99838fca6
Siyaf Khan <siyafk019@gmail.com>
2025-09-15 14:56:11 +0500

    Typescript Compiler option changed

--- frontend/src/pages/dashboard/dashboard-data-chart.tsx
+++ frontend/src/pages/dashboard/dashboard-data-chart.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+
 import { format } from "date-fns";
 import { Area, AreaChart, CartesianGrid, XAxis } from "recharts";
 import { useIsMobile } from "@/hooks/use-mobile";
